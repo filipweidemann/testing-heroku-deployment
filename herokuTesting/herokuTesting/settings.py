d71638be7b5238bc94396f1b6fed1806a0608a32
Filip Weidemann <filipweidemann@Filips-Air.fritz.box>
2018-01-07 16:19:07 +0100

    installed apps adding absolute path

--- herokuTesting/herokuTesting/settings.py
+++ herokuTesting/herokuTesting/settings.py
@@ -32,7 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'helloWorld.apps.HelloworldConfig',
+    'herokuTesting.helloWorld.apps.HelloworldConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
