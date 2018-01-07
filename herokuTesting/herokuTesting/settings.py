5ae41577563ca3de14be7194b022c6ba7c89e9c4
Filip Weidemann <filipweidemann@Filips-Air.fritz.box>
2018-01-07 16:21:00 +0100

    reverting..

--- herokuTesting/herokuTesting/settings.py
+++ herokuTesting/herokuTesting/settings.py
@@ -32,7 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'herokuTesting.helloWorld.apps.HelloworldConfig',
+    'helloWorld.apps.HelloworldConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
